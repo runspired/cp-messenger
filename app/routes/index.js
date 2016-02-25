b8b8a47c8281c49eab5bdfa9b02fe031c98711af
jhuang165 <programming-xcode@wireless-s1-no-150-28-192.uchicago.edu>
2016-02-24 20:31:32 -0600

    changes #2

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
-
-export default Ember.Route.extend({
-});
+export default Ember.Route.extend (
+    {
+        model( params ) {
+            return this.get ( 'store' ).find ( 'conversation' , params.id );
+        } ,
+        actions: {
+            sendMessage( text ) {
+                let user = this.get ( 'session.content.user' );
+                let conversation = this.modelFor ( 'conversations.single' );
+                this.get ( 'store' ).createRecord ( 'message' , {text , user , conversation} )
+                    .save ()
+                    .then (
+                        ( /* record */ ) => {
+                            conversation.save ();
+                        }
+                    );
+            }
+        }
+    }
+);

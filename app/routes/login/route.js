8adf15943c6971eb41606dfef2cad1ebbbb7ce78
Chris Thoburn <runspired@gmail.com>
2016-03-02 20:20:51 -0600

    fix auth

--- app/routes/login/route.js
+++ app/routes/login/route.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model (){
-		return this.get('store').create
-	}
+
+  model() {
+    return this.get('store').createRecord('user');
+  }
+
 });
